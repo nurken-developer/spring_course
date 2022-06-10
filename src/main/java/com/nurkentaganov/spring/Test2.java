c05ee77b3df6795d39289571dd54d578756b7574
nurken-developer <71592265+nurken-developer@users.noreply.github.com>
2022-06-11 01:47:35 +0600

    beans

--- src/main/java/com/nurkentaganov/spring/Test2.java
+++ src/main/java/com/nurkentaganov/spring/Test2.java
@@ -7,7 +7,9 @@ public class Test2 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-
+        Pet pet = applicationContext.getBean("myPet", Pet.class);
+        Pet cat = applicationContext.getBean("myCat",Pet.class);
+        cat.say();
 
 
         applicationContext.close();
